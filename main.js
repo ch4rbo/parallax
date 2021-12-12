88648be7c612f2f28ce393379760b06d8a5f8524
charbo <pulsemos@gmail.com>
2021-12-12 16:45:51 -0300

    cambio imagen, comento codigo | image change, add code description

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
     $(window).scroll(function(){
-        var barra = $(window).scrollTop();
-        var posicion = barra * 0.15;
+        var barra = $(window).scrollTop(); // target scroll //
+        var posicion = barra * 0.05; // efecto //
 
         $('body').css({
             'background-position': '0 -' + posicion + 'px'
